256381b4b8dc2825611b989bab56da4644b108d5
Rie Sugimoto <rs@isvr.soton.ac.uk>
2023-11-14 11:00:43 +0000

    Add something

--- climate_analysis.py
+++ climate_analysis.py
@@ -1,3 +1,5 @@
+"""Climate Analysis Tools"""
+
 import sys
 import temp_conversion
 import signal
@@ -25,3 +27,5 @@ for line in climate_data:
             kelvin = temp_conversion.fahr_to_kelvin(fahr)
 
             print(str(celsius)+", "+str(kelvin))
+
+# add call ??
